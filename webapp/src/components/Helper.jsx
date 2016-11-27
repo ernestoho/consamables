0f69742842d61c5103bf6f4c25fb59673494cf26
Sam Myers <sam@students.olin.edu>
2016-11-27 00:44:36 -0500

    Cleaned up some Sass. Helper text could still use some work.

--- webapp/src/components/Helper.jsx
+++ webapp/src/components/Helper.jsx
@@ -6,19 +6,21 @@ export default class Helper extends React.Component {
     render() {
         return (
             <div className="helper">
-                <div className="left-helper">
-                    <div className="helper-text">
-                        <div>Join an existing group</div>
-                        <div className="left-arrow">▼</div>
-                        <div className="right-arrow">▼</div>
-                    </div>
-                </div>
-                <div className="right-helper">
-                    <div className="helper-text">
-                        <div>Explore other options</div>
-                        <div className="left-arrow">▼</div>
-                        <div className="right-arrow">▼</div>
-                    </div>
+                <HelperText className="left-helper" text="Join an existing group"></HelperText>
+                <HelperText className="right-helper" text="Explore other options"></HelperText>
+            </div>
+        )
+    }
+}
+
+class HelperText extends React.Component {
+    render() {
+        return (
+            <div className={this.props.className}>
+                <div className="helper-text">{this.props.text}</div>
+                <div className="arrows">
+                    <div className="left-arrow">▼</div>
+                    <div className="right-arrow">▼</div>
                 </div>
             </div>
         )
