32cb24dc6ff66bfb8cd3f2a8c79f4dff8987b14a
Sam Myers <sam@students.olin.edu>
2016-12-20 13:32:07 -0800

    Fixed indentation.

--- webapp/src/components/Modal.jsx
+++ webapp/src/components/Modal.jsx
@@ -6,48 +6,48 @@ import { connect } from 'react-redux';
 import { hideModal } from '../actions';
 
 const modalStates = {
-	visible: {
-		visibility: 'visible',
-		opacity: 1
-	},
-	hidden: {
-		visibility: 'hidden',
-		opacity: 0
-	}
+    visible: {
+        visibility: 'visible',
+        opacity: 1
+    },
+    hidden: {
+        visibility: 'hidden',
+        opacity: 0
+    }
 }
 
 class Modal extends React.Component {
-	render() {
-		return (
-			<div
-				className="modal"
-				style={this.props.visible ? modalStates.visible : modalStates.hidden}
-				onClick={this.props.onClick}
-			>
-				<ModalContent/>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div
+                className="modal"
+                style={this.props.visible ? modalStates.visible : modalStates.hidden}
+                onClick={this.props.onClick}
+            >
+                <ModalContent/>
+            </div>
+        );
+    }
 }
 
 
 class ModalContent extends React.Component {
-	render() {
-		return (
-			<div className="modal-content">This is a modal</div>
-		);
-	}
+    render() {
+        return (
+            <div className="modal-content">This is a modal</div>
+        );
+    }
 }
 
 const mapStateToProps = state => ({visible: state.modal.get('visible')});
 
 const mapDispatchToProps = dispatch => {
-	return {
-		onClick: () => dispatch(hideModal())
-	};
+    return {
+        onClick: () => dispatch(hideModal())
+    };
 };
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Modal)
