c624356da838e0283ae6b4149680bedd7d85de55
Sam Myers <sam@students.olin.edu>
2016-12-25 23:59:39 -0800

    Added a live-updating order total.

--- webapp/src/components/panels/currentOrder/CurrentOrderPanel.jsx
+++ webapp/src/components/panels/currentOrder/CurrentOrderPanel.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 
 import PanelHeader from '../PanelHeader';
 import OrderItem from './OrderItem';
+import { getItemPrice } from '../../../selectors';
 
 class CurrentOrderPanel extends React.Component {
     render() {
@@ -14,14 +15,20 @@ class CurrentOrderPanel extends React.Component {
                 {this.props.items.map(([id, quantity]) => 
                     <OrderItem key={id} id={id} quantity={quantity}/>
                 )}
-                <button className="button">Continue</button>
+                <div className="continue">
+                    <button className="button">Continue</button>
+                    <div className="order-total">Total: ${this.props.totalCost.toFixed(2)}</div>
+                </div>
             </div>
         );
     }
 }
 
 const mapStateToProps = state => ({
-    items: state.centerColumn.get('orderItems').entrySeq().toJS()
+    items: state.centerColumn.get('orderItems').entrySeq().toJS(),
+    totalCost: state.centerColumn.get('orderItems').reduce((total, quantity, id) => {
+        return total += getItemPrice(state, id) * quantity;
+    }, 0)
 });
 
 export default connect(
