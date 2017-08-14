daecc8409c0eb8a9581af31aa5dbac38ed05b071
Sam Myers <sam@students.olin.edu>
2017-08-14 00:07:34 -0400

    Fix id parsing HOC.

--- webapp/src/common/utils/parseId.jsx
+++ webapp/src/common/utils/parseId.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default WrappedComponent => {
   const withIntId = props => (
   // eslint-disable-next-line
-    <WrappedComponent {...props} id={parseInt(props.params.id, 10)} />
+    <WrappedComponent {...props} id={parseInt(props.id || props.params.id, 10)} />
   );
 
   withIntId.displayName = `ParsedId(${WrappedComponent.displayName || WrappedComponent.name})`;
